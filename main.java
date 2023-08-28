5ecc1fe512e71e59508ebf3bf3b00846bf47524e
idangelo <idangelo@andreani.com>
2023-08-28 14:19:16 -0300

    Cambio de mensaje

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class Main{
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
-		System.out.println("Nuevo commit realizado");
+		System.out.println("Nuevo commit se a hecho!!!");
 	}
 }
